deaf09afe68e41e0441b3b1c2bd7143011c54f5c
SewerSnake <ericgroseclos@hotmail.com>
2018-01-13 14:52:16 +0100

    Started working with changing background color via the press of a button.

--- App_About_Me/CocoaViewController.m
+++ App_About_Me/CocoaViewController.m
@@ -10,28 +10,61 @@
 
 @interface CocoaViewController ()
 
+@property (weak, nonatomic) IBOutlet UIButton *colorButton;
+
 @end
 
 @implementation CocoaViewController
 
+// Retrieves an integer from NSUserDefaults.
+// Changes the background of the app to the saved color.
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    NSUserDefaults *preferences = [NSUserDefaults standardUserDefaults];
+    
+    NSString *currentColorKey = @"currentcolor";
+    
+    if (!([preferences objectForKey:currentColorKey] == nil))
+    {
+        const NSInteger color = [preferences integerForKey:currentColorKey];
+        [self applyColor:(int)color];
+    }
+    
 }
+// Changes the color to red, blue or green.
+- (void)applyColor: (int)number {
+    if (number == 1) {
+        self.view.backgroundColor = [UIColor redColor];
+    } else if (number == 2) {
+        self.view.backgroundColor = [UIColor blueColor];
+    } else if (number == 3) {
+        self.view.backgroundColor = [UIColor greenColor];
+    }
+}
+
+// Changes the background color for
+// the entire app when the button is pressed. The color is
+// decided randomly.
+- (IBAction)colorChange:(id)sender {
+    int number = 1 + arc4random_uniform(3);
+    [self applyColor:number];
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
-/*
+
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    // Gets the new view controller
+    //[segue destinationViewController]
+    // Passes the selected object to the new view controller.
 }
-*/
+
 
 @end
